fc7b2d3d677e317ccb392b0e2a992c31b6361103
Axel Svensson <mail@axelsvensson.com>
2019-08-18 15:48:05 +0200

    improve session startup
    - session-init
      - restructure
      - add comments
      - create links to special folders in parallell
      - always finish link creation before shell startup
    - add comments in Cygwin.bat
    - remove initial session-chcp and session-id

--- cygwin/portable-cygwin/Cygwin.bat
+++ cygwin/portable-cygwin/Cygwin.bat
@@ -1,15 +1,27 @@
 @echo off
+
+rem Go to root directory
 %~d0
 cd %~dp0
 cd ..
+
+rem Remember active codepage
 chcp > portable-cygwin\session-chcp
+
+rem Run session initialization script
 bin\bash.exe -i /portable-cygwin/session-init
+
+rem Use mintty if it exists, otherwise cmd
 if exist bin\mintty.exe goto minttyexists
 goto minttydoesntexist
+
+rem Start a shell using mintty
 :minttyexists
 cmd /c start bin\mintty.exe -
 if errorlevel 1 goto helpmintty
 goto quit
+
+rem If mintty failed, setup the environment and retry
 :helpmintty
 echo Something went wrong starting mintty.
 echo Trying to setup the environment before starting mintty.
@@ -18,8 +30,11 @@ HOME=/home
 cd home
 cmd /c start ..\bin\mintty.exe
 goto quit
+
+rem If mintty doesn't exist, start a shell using cmd
 :minttydoesntexist
 echo Mintty isn't installed.
 echo Running bash in the command window.
 bin\bash.exe --login -i
+
 :quit
